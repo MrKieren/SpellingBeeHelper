63711a5d8657c28a22f50ab2d4ffc1d6cc610eb5
Kieren Searle <kierensearle@gmail.com>
2023-03-17 11:30:02 -0400

    Add a simple word counter
    Add a simple word counter that counts up the number of single and
    double letters and prints them to the screen

--- src/content_scripts/spellingbee.tsx
+++ src/content_scripts/spellingbee.tsx
@@ -1,17 +1,88 @@
-import { ReactElement } from "react"
+import { useState } from "react"
 import ReactDOM from "react-dom"
 
 export const SPELLING_BEE_CONTENT_AREA = "spelling-bee-content-area"
 
 const SpellingBee = () => {
+    const [letterMap, setLetterMap] = useState(new Map<string, number>())
+    const [doubleLetterMap, setDoubleLetterMap] =
+        useState(new Map<string, Map<string, number>>())
+
+    const UpdateWords = () => {
+        const wordList = document.getElementsByClassName("sb-has-words")
+        if (wordList == null) {
+            console.log("Failed to find word list.")
+            return
+        }
+
+        const words = wordList[0].getElementsByClassName("sb-anagram")
+        if (words == null) {
+            console.log("Failed to find words in word list.")
+            return
+        }
+
+        const singleLetterWords = new Map<string, number>();
+        const doubleLetterWords = new Map<string, Map<string, number>>();
+
+        for (let index = 0; index < words.length; index++) {
+            const child = words[index]
+
+            const word = child.innerHTML
+            const firstLetter = word[0]
+            const first2Letters = word[0] + word[1]
+
+            if (!singleLetterWords.has(firstLetter)) {
+                singleLetterWords.set(firstLetter, 0)
+            }
+            singleLetterWords.set(
+                firstLetter, singleLetterWords.get(firstLetter)! + 1
+            )
+
+            if (!doubleLetterWords.has(firstLetter)) {
+                doubleLetterWords.set(firstLetter, new Map<string, number>())
+            }
+            if (!doubleLetterWords.get(firstLetter)!.get(first2Letters)) {
+                doubleLetterWords.get(firstLetter)!.set(first2Letters, 0)
+            }
+            doubleLetterWords.get(firstLetter)!.set(
+                first2Letters,
+                doubleLetterWords.get(firstLetter)!.get(first2Letters)! + 1
+            )
+        }
+
+        setLetterMap(singleLetterWords)
+        setDoubleLetterMap(doubleLetterWords)
+    }
+
+    const DoubleLetterMapToString = (map: Map<string, number>) => {
+        return <>
+            {Array.from(map).sort().map(([key, value]) => (
+                <>
+                    <div>&emsp;{key}: {value}</div>
+                </>
+            ))}
+        </>
+    }
+
     return (
-        <div>
-            <span className="spelling-bee-helper-header">Spelling Bee Helper</span>
-        </div>
+        <>
+            <span className="spelling-bee-helper-header">
+                Spelling Bee Helper
+            </span>
+
+            {Array.from(letterMap).sort().map(([key, value]) => (
+                <>
+                    <div>{key}: {value}</div>
+                    {DoubleLetterMapToString(doubleLetterMap.get(key)!)}
+                </>
+            ))}
+
+            <button onClick={UpdateWords}>Update</button>
+        </>
     )
 }
 
-const Render = (dom: ReactElement) => {
+const Render = () => {
     const contentBox = document.getElementsByClassName("sb-content-box")
     if (contentBox == null || contentBox.length !== 1) {
         console.log("Failed to find content box.")
@@ -25,12 +96,12 @@ const Render = (dom: ReactElement) => {
     contentBox[0].appendChild(wordListWindow)
 
     ReactDOM.render(
-        dom,
+        <SpellingBee />,
         document.getElementById(SPELLING_BEE_CONTENT_AREA)
     )
 }
 
 console.log("Loading Spelling Bee Helper.")
-Render(SpellingBee())
+Render()
 
 export default SpellingBee
