a0965ac79e4eb794d82f1b5abd1df51d84d35938
Kieren Searle <kierensearle@gmail.com>
2023-03-19 11:22:45 -0400

    Add Spelling Bee Grid
    Added the Spelling Bee Grid to the main page, and show a live count
    when the button is clicked

--- src/content_scripts/spellingbee.tsx
+++ src/content_scripts/spellingbee.tsx
@@ -4,11 +4,19 @@ import ReactDOM from "react-dom"
 export const SPELLING_BEE_CONTENT_AREA = "spelling-bee-content-area"
 
 const SpellingBee = () => {
-    const [letterMap, setLetterMap] = useState(new Map<string, number>())
-    const [doubleLetterMap, setDoubleLetterMap] =
-        useState(new Map<string, Map<string, number>>())
+    const [requiredWordLengths, setRequiredWordLengths] =
+        useState(new Array<number>())
+    const [requiredLetterCounts, setRequiredLetterCounts] =
+        useState(new Map<string, Map<number, number>>())
+    const [foundLetterCounts, setFoundLetterCounts] =
+        useState(new Map<string, Map<number, number>>())
 
-    const UpdateWords = () => {
+    const Update = async () => {
+        await FetchData()
+        UpdateFoundWords()
+    }
+
+    const UpdateFoundWords = () => {
         const wordList = document.getElementsByClassName("sb-has-words")
         if (wordList == null) {
             console.log("Failed to find word list.")
@@ -21,65 +29,198 @@ const SpellingBee = () => {
             return
         }
 
-        const singleLetterWords = new Map<string, number>();
-        const doubleLetterWords = new Map<string, Map<string, number>>();
+        const letterCounts = new Map<string, Map<number, number>>()
 
         for (let index = 0; index < words.length; index++) {
             const child = words[index]
 
             const word = child.innerHTML
             const firstLetter = word[0]
-            const first2Letters = word[0] + word[1]
 
-            if (!singleLetterWords.has(firstLetter)) {
-                singleLetterWords.set(firstLetter, 0)
+            if (!letterCounts.has(firstLetter)) {
+                letterCounts.set(firstLetter, new Map<number, number>())
             }
-            singleLetterWords.set(
-                firstLetter, singleLetterWords.get(firstLetter)! + 1
-            )
 
-            if (!doubleLetterWords.has(firstLetter)) {
-                doubleLetterWords.set(firstLetter, new Map<string, number>())
+            if (!letterCounts.get(firstLetter)?.get(word.length)) {
+                letterCounts.get(firstLetter)?.set(word.length, 0)
             }
-            if (!doubleLetterWords.get(firstLetter)!.get(first2Letters)) {
-                doubleLetterWords.get(firstLetter)!.set(first2Letters, 0)
-            }
-            doubleLetterWords.get(firstLetter)!.set(
-                first2Letters,
-                doubleLetterWords.get(firstLetter)!.get(first2Letters)! + 1
-            )
+
+            const count = letterCounts.get(firstLetter)?.get(word.length) ?? 0
+            letterCounts.get(firstLetter)?.set(word.length, count + 1)
         }
 
-        setLetterMap(singleLetterWords)
-        setDoubleLetterMap(doubleLetterWords)
+        setFoundLetterCounts(letterCounts)
     }
 
-    const DoubleLetterMapToString = (map: Map<string, number>) => {
+    const FetchData = async () => {
+        try {
+            const response = await fetch(
+                `https://www.nytimes.com/${CurrentDate()}/crosswords/spelling-bee-forum.html`
+            )
+            if (!response.ok) {
+                throw new Error(`HTTPS error status: ${response.status}`)
+            }
+
+            const html = await response.text()
+
+            const parser = new DOMParser()
+            const doc = parser.parseFromString(html, "text/html")
+
+            const interactiveBody =
+                doc.getElementsByClassName("interactive-body")
+            if (interactiveBody == null || interactiveBody.length !== 1) {
+                console.log("Failed to find interactive body.")
+                return
+            }
+
+            ParseSpellingBeeGrid(interactiveBody[0])
+            ParseTwoLetterList(interactiveBody[0])
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const CurrentDate = () => {
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = String(today.getMonth() + 1).padStart(2, "0");
+        const day = String(today.getDate()).padStart(2, "0");
+
+        return `${year}/${month}/${day}`;
+    }
+
+    const ParseSpellingBeeGrid = (interactiveBody: Element) => {
+        const grid = interactiveBody.getElementsByTagName("table")
+        if (grid == null || grid.length !== 1) {
+            console.log("Failed to find Spelling Bee Grid.")
+            return [new Set<number>(), new Map<string, Array<number>>()]
+        }
+
+        const rows = grid[0].getElementsByTagName("tr")
+
+        const wordLengths = new Array<number>()
+
+        const wordCountCells = rows[0].getElementsByTagName("td")
+        for (let i = 0; i < wordCountCells.length; i++) {
+            const data = wordCountCells[i].textContent?.trim() ?? ""
+            if (data === "-") {
+                wordLengths.push(0)
+            } else if (data.length > 0 && !isNaN(Number(data))) {
+                wordLengths.push(Number(data))
+            }
+        }
+
+        const letterCounts = new Map<string, Map<number, number>>()
+
+        for (let i = 1; i < rows.length; i++) {
+            const row = rows[i].getElementsByTagName("td")
+            const rowLetter = row[0].textContent?.trim().replace(":", "") ?? ""
+            if (rowLetter?.length === 0) {
+                console.log("Failed to get row letter.")
+                continue
+            }
+
+            letterCounts.set(rowLetter, new Map<number, number>())
+
+            for (let j = 1; j < row.length; j++) {
+                const data = row[j].textContent?.trim() ?? ""
+                if (data === "-") {
+                    letterCounts.get(rowLetter)!.set(wordLengths[j - 1], 0)
+                } else if (data.length > 0 && !isNaN(Number(data))) {
+                    letterCounts
+                        .get(rowLetter)!
+                        .set(wordLengths[j - 1], Number(data))
+                }
+            }
+        }
+
+        setRequiredWordLengths(wordLengths)
+        setRequiredLetterCounts(letterCounts)
+    }
+
+    const ParseTwoLetterList = (interactiveBody: Element) => {
+        const pElements = interactiveBody.getElementsByTagName("p")
+        if (pElements === null || pElements.length === 0) {
+            console.log("Failed to find two letter list.")
+            return new Map<string, number>()
+        }
+
+        const twoLetterList = pElements[pElements.length - 1]
+            .getElementsByTagName("span")
+
+        const twoLetterCounts = new Map<string, number>()
+        for (let i = 0; i < twoLetterList.length; i++) {
+            const data = twoLetterList[i].textContent?.trim() ?? ""
+            const splitData = data.split(" ")
+
+            for (let j = 0; j < splitData.length; j++) {
+                const itemCount = splitData[j].split("-")
+                if (itemCount.length !== 2) {
+                    console.log("Invalid two letter count: " + splitData[j])
+                    continue
+                }
+
+                twoLetterCounts.set(itemCount[0], Number(itemCount[1]))
+            }
+        }
+
+        return twoLetterCounts
+    }
+
+    const CalculateWordCount = () => {
+        const updatedCounters = new Map<string, Map<number, number>>()
+
+        requiredLetterCounts.forEach((counts, letter) => {
+            const updatedLetterCounts = new Map<number, number>()
+            counts.forEach((count, wordLength) => {
+                const foundCount =
+                    foundLetterCounts.get(letter)?.get(wordLength) ?? 0
+                updatedLetterCounts.set(wordLength, count - foundCount)
+            })
+
+            updatedCounters.set(letter, updatedLetterCounts)
+        })
+
         return <>
-            {Array.from(map).sort().map(([key, value]) => (
+            {Array.from(updatedCounters).sort().map(([letter, counts]) => (
                 <>
-                    <div>&emsp;{key}: {value}</div>
+                    <tr>
+                        <td className="cell">{letter}</td>
+                        {Array.from(counts).map(([wordLength, count]) => (
+                            <td className="cell">{count}</td>
+                        ))}
+                    </tr>
                 </>
             ))}
         </>
     }
 
-    return (
-        <>
-            <span className="spelling-bee-helper-header">
-                Spelling Bee Helper
-            </span>
+    const SpellingBeeGrid = () => {
+        return <table className="table">
+            <tbody>
+                <tr className="row">
+                    <>
+                        <td className="cell"></td>
+                        {Array.from(requiredWordLengths).map(value => (
+                            <td className="cell">{value}</td>
+                        ))}
+                    </>
+                    <td className="cell">Σ</td>
+                </tr>
+                <CalculateWordCount />
+            </tbody>
+        </table>
+    }
 
-            {Array.from(letterMap).sort().map(([key, value]) => (
-                <>
-                    <div>{key}: {value}</div>
-                    {DoubleLetterMapToString(doubleLetterMap.get(key)!)}
-                </>
-            ))}
+    return <>
+        <span className="spelling-bee-helper-header">
+            Spelling Bee Helper
+        </span>
 
-            <button onClick={UpdateWords}>Update</button>
-        </>
-    )
+        <SpellingBeeGrid />
+
+        <button onClick={Update}>Update</button>
+    </>
 }
 
 const Render = () => {
