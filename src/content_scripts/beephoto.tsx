6b3f55197dce45111c5aeec2bcecb05fcb8699b3
Kieren <kierensearle@gmail.com>
2023-04-28 15:02:21 -0400

    Show full sized bee image
    When the user clicks the bee image it is now expanded to show the
    full sized version

--- src/content_scripts/beephoto.tsx
+++ src/content_scripts/beephoto.tsx
@@ -1,24 +1,78 @@
+import { useEffect, useState } from "react";
+
 type Props = {
     src: string,
     srcset: string,
     credit: string
 }
 
+const DefaultPictureWidth = 1024
+
 const BeePhoto = ({
     src,
     srcset,
     credit
 }: Props) => {
+    const [width, setWidth] = useState(DefaultPictureWidth)
+
+    const [isOverlayVisible, setIsOverlayVisible] = useState(false)
+
+    function showBeePictureOverlay() {
+        setIsOverlayVisible(true)
+    }
+
+    function hideBeePictureOverlay() {
+        setIsOverlayVisible(false)
+    }
+
+    useEffect(() => {
+        if (isOverlayVisible) {
+            document.addEventListener("click", hideBeePictureOverlay)
+        } else {
+            document.removeEventListener("click", hideBeePictureOverlay)
+        }
+
+        return () => {
+            document.removeEventListener("click", hideBeePictureOverlay)
+        }
+    }, [isOverlayVisible])
+
+    useEffect(() => {
+        setWidth(
+            parseInt(srcset.split(" ").pop() ?? DefaultPictureWidth.toString())
+        )
+    }, [srcset])
+
     return <>
         <div>
-            <img
-                className="spelling-bee-helper-bee-photo"
-                src={src}
-                srcSet={srcset}
-                alt={credit} />
-            <p className="spelling-bee-helper-bee-photo-credit">
-                {credit}
-            </p>
+            <div onClick={showBeePictureOverlay}>
+                <img
+                    className="spelling-bee-helper-bee-photo-thumbnail"
+                    src={src}
+                    srcSet={srcset}
+                    alt={credit} />
+                <p className="spelling-bee-helper-bee-photo-credit">
+                    {credit}
+                </p>
+            </div>
+
+            {isOverlayVisible && (
+                <div className="spelling-bee-helper-bee-photo-overlay">
+                    <div className="spelling-bee-helper-bee-photo-overlay-content">
+                        <p className="spelling-bee-helper-bee-photo-overlay-close">
+                        </p>
+                        <img
+                            className="spelling-bee-helper-bee-photo-overlay-photo"
+                            src={src}
+                            srcSet={srcset}
+                            alt={credit}
+                            width={width} />
+                        <p className="spelling-bee-helper-bee-photo-credit-overlay">
+                            {credit}
+                        </p>
+                    </div>
+                </div>
+            )}
         </div>
     </>
 }
