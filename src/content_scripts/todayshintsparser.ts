b636461b0d1b18d0c5346f32338adcd213ffa5ff
Kieren Searle <kierensearle@gmail.com>
2024-08-25 15:45:49 -0400

    Fix 403 errors when downloading today's hints
    Fixed a problem where NYT was rejecting requests for the today's hints due to missing headers

--- src/content_scripts/todayshintsparser.ts
+++ src/content_scripts/todayshintsparser.ts
@@ -26,8 +26,18 @@ type BeePhoto = {
 
 export async function fetchData(): Promise<TodaysHintsData> {
     const response = await fetch(
-        `https://www.nytimes.com/${getGameDate()}/crosswords/spelling-bee-forum.html`
-    )
+    `https://www.nytimes.com/${getGameDate()}/crosswords/spelling-bee-forum.html`,
+    {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+        Referer: "https://www.nytimes.com/",
+      }
+    }
+  );
     if (!response.ok) {
         throw new Error(
             `Failed to access Today's Hints (error ${response.status}).`
